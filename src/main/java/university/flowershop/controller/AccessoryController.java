3b4ceb0dc5c4e80a43908e547ee0d6ec27382145
seo-sang-ho <rocter2049@naver.com>
2023-03-20 00:33:01 +0900

    코드 간결하게 요약

--- src/main/java/university/flowershop/controller/AccessoryController.java
+++ src/main/java/university/flowershop/controller/AccessoryController.java
@@ -4,20 +4,11 @@ package university.flowershop.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import university.flowershop.domain.Cart;
-import university.flowershop.domain.CartItem;
 import university.flowershop.domain.item.Accessory;
-import university.flowershop.domain.item.Flower;
-import university.flowershop.repository.FlowerRepository;
 import university.flowershop.service.AccessoryService;
-import university.flowershop.service.FlowerService;
-
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -27,14 +18,14 @@ public class AccessoryController {
 
     private final AccessoryService accessoryService;
 
-    @GetMapping("/accessory/new") // 꽃 상품 추가 화면으로 간다
+    @GetMapping("/accessory/new")
     public String createFlowerForm(Model model) {
         model.addAttribute("accessoryForm", new AccessoryForm());
         return "products/addAccessoryForm";
     }
 
-    @PostMapping("/accessory/new") // 꽃 상품 추가 로직 실행
-    public String createAccessory(@Valid AccessoryForm form, BindingResult bindingResult, Model model) {
+    @PostMapping("/accessory/new")
+    public String createAccessory(@Valid AccessoryForm form) {
 
         Accessory accessory = new Accessory();
         accessory.setPrdNum(form.getPrdNum());
@@ -60,7 +51,5 @@ public class AccessoryController {
         model.addAttribute("accessory", accessory);
         return "products/accessoryDetail";
     }
-
-
 
 }
